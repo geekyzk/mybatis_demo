32cf3792906b9cef2a866138104034f6942f3883
geekyzk <geekyzk@163.com>
2017-05-02 19:36:54 +0800

    Initial commit

---
+++ src/main/java/com/em248/Application.java
@@ -0,0 +1,31 @@
+package com.em248;
+
+import com.em248.pojo.Photo;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by tian on 2017/5/2.
+ */
+public class Application {
+
+
+    @Test
+    public void test() throws IOException {
+
+        InputStream inputStream = Resources.getResourceAsStream("mybatisConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Photo o = (Photo)sqlSession.selectOne("com.em248.dao.PhotoMapper","4fcd1a6d-67f7-4929-8b60-e446c8c015be");
+        System.out.println(o.getName());
+    }
+
+
+
+}
